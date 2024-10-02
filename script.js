c35fad8297afe067ff598c78598fbf9a5d19c1fe
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-02 10:24:35 +0300

    Global object 1

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+/* setTimeout(function(){
+
+    console.log('3 seconds have passed');
+
+}, 3000) */
+
+let counter = 0
+
+/* setInterval(function(){
+
+    counter++
+
+    console.log(counter)
+
+}, 2000) */
+
+
+const timer = setInterval(function(){
+
+    counter++
+
+    console.log(counter)
+
+    if(counter >= 5){
+
+        clearInterval(timer)
+
+    }
+
+}, 2000)
