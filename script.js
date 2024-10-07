f95b11f5956e3715efc273adc86c09721389d104
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-07 15:18:06 +0300

    Writing a file async

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ import fs from "fs"
 
 const readMe = fs.readFile("README.md", "utf-8", (err, data) =>{
     
-    console.log(data)
+    fs.writeFile("nodejs.txt", data, err =>{
+
+        if(err){
+
+            console.log(err)
+ 
+        }
+
+    })
 
 })
