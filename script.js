9421443c39e50e27b1402f7d3d2c52c8e813ef06
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-07 14:36:53 +0300

    Reading a file

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-import EventEmitter from "events"
-import util from "util"
+import fs from "fs"
 
-class Person{
+const readMe = fs.readFileSync("README.md", "utf-8")
 
-    constructor(name){
-
-        this.name = name
-
-    }
-
-}
-
-util.inherits(Person, EventEmitter)
-
-
-const ben = new Person('Ben Franklin'),
-      tom = new Person('Tom Hanks'),
-      mary = new Person('Mary Poppins'),
-
-      people = [ben, tom, mary]
-
-people.forEach(person =>{
-
-    person.on("speak", (message) =>{
-
-        console.log(person.name + ' said: ' + message)
-
-    })
-
-})
-
-ben.emit("speak", 'Hello, I am Ben Franklin')
-tom.emit("speak", 'Hello, I am Tom Hanks')
-mary.emit("speak", 'Hello, I am Mary Poppins')
+console.log(readMe)
