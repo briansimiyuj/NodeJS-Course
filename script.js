723553a8b8caadebabb91f242f49732415f85812
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-08 11:09:42 +0300

    Deleting a directory async

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 import fs from "fs"
 
-fs.mkdir("newFolder", err =>{
-
+fs.rm("newFolder", { recursive: true }, err =>{
+   
     if(err) console.log(err)
 
-    fs.readFile("README.md", "utf-8", (err, data) =>{
-
-        if(err) console.log(err)
-
-        fs.writeFile("newFolder/index.md", data, err =>{
-
-            if(err) console.log(err)
-
-            console.log('File created')
-
-        })
-
-    })
+    console.log('folder deleted')
 
 })
