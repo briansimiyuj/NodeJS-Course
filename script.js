7d107f08f66f20814929213f63747a6d1a3f6317
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-09 12:06:03 +0300

    Creating a server 2

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-import { HTTP } from "http"
+import http from "http"
 
-const server = HTTP.createServer((request, response) =>{
+const server = http.createServer((request, response) =>{
 
-   
+    console.log('Request was made:' + request.url)
+
+   response.writeHead(200, {"Content-Type": "text/plain"}) 
+
+   response.end('Hello World')
+
+})
+
+server.listen(3000, ("127.0.0.1"), () =>{
+
+    console.log('Server is running on port 3000')
 
 })
