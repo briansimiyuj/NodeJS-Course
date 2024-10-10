7eae265f84172ff70d437022528e28cca6098d50
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-10 14:18:40 +0300

    Readable streams

--- README.md
+++
@@ -1,3 +0,0 @@
-This is a Node.js course Taught by The Net Ninja
-It was published in 2016
-I'm learning it in 2024
---
+++ script.js
@@ -0,0 +1,17 @@
+import http from "http"
+import fs from "fs"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+
+const __filename = fileURLToPath(import.meta.url),
+      __dirname = dirname(__filename)
+
+const myReadStream = fs.createReadStream(__dirname + "/README.txt", "utf8")
+
+myReadStream.on("data", (chunk) =>{
+
+    console.log('new chunk received:')
+
+    console.log(chunk)
+
+})
