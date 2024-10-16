98afd4f49bbebd6c29ecfc2d4cb2c63a5125b651
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-16 11:24:27 +0300

    Piping streams 2

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 import fs from "fs"
+import http from "http"
 import { fileURLToPath } from "url"
 import { dirname } from "path"
 
 const __filename = fileURLToPath(import.meta.url),
       __dirname = dirname(__filename)
 
-const myReadStream = fs.createReadStream(__dirname + "/README.txt", "utf8"),
-      myWriteStream = fs.createWriteStream(__dirname + "/output.txt")
 
-myReadStream.pipe(myWriteStream)
+const server = http.createServer((request, response) =>{
+
+    console.log('Request was made:' + request.url)
+
+    response.writeHead(200, {"Content-Type": "text/plain"}) 
+
+    const myReadStream = fs.createReadStream(__dirname + "/README.txt", "utf8")
+
+    myReadStream.pipe(response)
+
+})
+
+server.listen(3000, ("127.0.0.1"), () =>{
+
+    console.log('Server is running on port 3000')
+
+})
