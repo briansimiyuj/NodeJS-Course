57491363c0ddc978987a02d30092f26eff6f2d00
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-16 09:55:23 +0300

    Writable streams

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ import { dirname } from "path"
 const __filename = fileURLToPath(import.meta.url),
       __dirname = dirname(__filename)
 
-const myReadStream = fs.createReadStream(__dirname + "/README.txt", "utf8")
+const myReadStream = fs.createReadStream(__dirname + "/README.txt", "utf8"),
+      myWriteStream = fs.createWriteStream(__dirname + "/writeMe.txt")
 
 myReadStream.on("data", (chunk) =>{
 
     console.log('new chunk received:')
 
-    console.log(chunk)
+    myWriteStream.write(chunk)
 
 })
