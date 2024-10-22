3d76fc02a0c63629f92b2eedc7fee089a7f3ef5a
Brian Simiyu <briansimiyuj@gmail.com>
2024-10-22 11:43:07 +0300

    Using express

---
+++ script.js
@@ -0,0 +1,21 @@
+import express from "express"
+
+const app = express()
+
+app.listen(3000, () =>{
+
+    console.log('Server is running on port 3000')
+   
+    app.get("/", (request, response) =>{
+
+        response.send('Hello World')
+
+    })
+
+    app.get("/about", (request, response) =>{
+
+        response.send('This is the about page')
+
+    })
+
+})
